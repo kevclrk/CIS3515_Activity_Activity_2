25eb1672c0924cfd973374eef8dd8782af81eb83
kevclrk <kevin.clark2404@gmail.com>
2024-10-03 14:47:58 -0400

    completed to do 1

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+
+const val RESULT_KEY = "reply message"
 
 class DisplayActivity : AppCompatActivity() {
 
@@ -12,6 +15,14 @@ class DisplayActivity : AppCompatActivity() {
     // TODO Step 3: Use returned value for lyricsDisplayTextView text size
 
     private lateinit var lyricsDisplayTextView: TextView
+
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            it.data?.apply {
+                lyricsDisplayTextView.text = getStringExtra(RESULT_KEY)
+            }
+        }
+    }
     private lateinit var textSizeSelectorButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
